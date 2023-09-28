9c3cd53141654b59e87c706d31cd4ac7b01c3575
yu-heejin <jkgt0521@naver.com>
2023-09-28 22:27:25 +0900

    :sparkles: [3주차] 네트워크

--- Live-Coding-Test/네트워크.java
+++ Live-Coding-Test/네트워크.java
@@ -1,33 +1,40 @@
 import java.util.*;
 
-// 미완성 코드 ㅠㅠ
-
 class Solution {
     
     private int[] parent;
     
     public int solution(int n, int[][] computers) {
-        parent = new int[n + 1];
-        for (int i = 0; i < parent.length; i++) {
+        parent = new int[n];
+        
+        for (int i = 0; i < n; i++) {
             parent[i] = i;
         }
         
         // 연결되었는지 확인
-        
-        // 같은 네트워크끼리 묶음
-        Map<Integer, Integer> results = new HashMap<>();
-        for (int i = 1; i <= n; i++) {
-            int cnt = results.getOrDefault(parent[i], 0);
-            results.put(parent[i], cnt + 1);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j && computers[i][j] == 1) {
+                    union(i, j);
+                }
+            }
         }
-        return results.size();
+        
+        // Map이랑 Set으로할 땐 답이 안나왔는데 왜 이렇게 하니까 답이 나올까요..?
+        int size = 0;
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == i) {
+                size++;
+            }
+        }
+        
+        return size;
     }
     
     private boolean union(int x, int y) {
-        x = find(x); //x의 부모노드 찾기
-        y = find(y); //y의 부모노드 찾기
+        x = find(x); // x의 부모노드 찾기
+        y = find(y); // y의 부모노드 찾기
 
-        // 이미 같은 그래프에 속해있을 때 false 반환
         if(x == y) return false;
 
         if(x <= y) parent[y] = x;
